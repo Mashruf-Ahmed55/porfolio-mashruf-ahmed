3f8ca6770b9296a4869849d4922e2109354e74a9
mashruf-ahmed55 <mashrufahmed03@gmail.com>
2025-06-30 13:35:11 +0600

    add email api

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,55 @@
+import AdminNotification from '@/components/templates/AdminNotification';
+import UserConfirmation from '@/components/templates/UserConfirmation';
+import { render } from '@react-email/render';
+import { NextRequest, NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const { name, email, subject, message } = await req.json();
+
+    if (!name || !email || !message) {
+      return NextResponse.json(
+        { success: false, error: 'All fields required' },
+        { status: 400 }
+      );
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    // 📨 1. Mail to YOU
+    await transporter.sendMail({
+      from: `"Portfolio Contact" <${process.env.EMAIL_USER}>`,
+      to: process.env.EMAIL_USER,
+      replyTo: email,
+      subject: subject || `New message from ${name}`,
+      html: await render(AdminNotification({ name, email, message })),
+    });
+
+    await transporter.sendMail({
+      from: `"Mashruf Ahmed" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: 'We received your message!',
+      html: await render(UserConfirmation({ name })),
+    });
+
+    return NextResponse.json(
+      { success: true, message: 'Message sent successfully' },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error('Contact API Error:', error);
+    return NextResponse.json(
+      { success: false, error: 'Something went wrong' },
+      { status: 500 }
+    );
+  }
+};
